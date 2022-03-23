6875525c101fc5192d66c6f900f1575d3de0cb48
Frezirovshik <satanist99999@mail.ru>
2022-03-23 18:08:55 +0500

    Добавьте файлы проекта.

--- T/Form1.cs
+++ T/Form1.cs
@@ -34,7 +34,6 @@ namespace T
         private Button button2;
         private Button button3;
         int iter = 0;    // счетчик точек при вводе
-        private Button button4;
         private Label label1;
         Pen pen = new Pen(Color.Black);
         private void Form1_MouseClick(object sender, MouseEventArgs e)
@@ -215,7 +214,6 @@ namespace T
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -249,15 +247,6 @@ namespace T
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(13, 225);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 65);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Очистить";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -271,7 +260,6 @@ namespace T
             // 
             this.ClientSize = new System.Drawing.Size(927, 533);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
